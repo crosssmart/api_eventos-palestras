2b67174ef787f485031ed0ff4c2eecf5aeff6a0a
Rogerio Filho <rogerio.rhty@outlook.com>
2026-06-18 10:42:47 -0400

    usuario controller modify

---
+++ controller/usuario.js
@@ -0,0 +1,38 @@
+const Produtos = require('../model/usuario.js');
+const bcrypt = require('bcrypt');
+
+exports.registrarUsuario = async (req, res) => {
+  const nome = req.body.nome;
+  const cpf = req.body.cpf;
+  const email = req.body.email;
+  const senha = req.body.senha;
+  const pronomes = req.body.pronomes;
+
+  if (!nome || !cpf || !email || !senha || !pronomes) {
+    return res.send({ msg: '[ERRO]: Informar dados corretamente!' });
+  }
+
+  try {
+    const usuarioJahExiste = await Usuario.findOne({ email: email });
+    if (usuarioJahExiste) {
+      return res.send({ msg: '[ERRO]: Já existe um usuário com esse email!' });
+    }
+
+    const senhaEncriptada = await bcrypt.hash(senha, 10);
+
+    const novoUsuario = {
+      nome: nome,
+      cpf: cpf,
+      email: email,
+      senha: senhaEncriptada,
+      pronomes: pronomes
+    }
+
+    await Usuario.create(novoUsuario);
+    res.send('[SUCESSO]: Usuario criado com sucesso!');
+
+  } catch (erro) {
+    console.log(erro);
+    res.send({ msg: '[ERRO]: Erro ao cadastrar usuario', detalhes: erro });
+  }
+}
